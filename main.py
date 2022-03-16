528968a3bdd50151a8165c55e405e0654e7dbc5b
5kept1k <101666806+5kept1k@users.noreply.github.com>
2022-03-16 00:38:48 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+source = 'C:\\Users\\allan\\OneDrive\\Desktop\\FolderA\\File0314.txt'
+destination = 'C:\\Users\\allan\\OneDrive\\Desktop\\FolderB\\File0316.txt'
+try:
+    os.rename(source,destination)
+    print(source+' has been successfully moved from FolderA to FolderB!')
+except FileNotFoundError:
+    os.rename(destination,source)
+    print(source+' has been successfully moved from FolderB to FolderA!')
